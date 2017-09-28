7b18417392e2ac009aa516fbca262812e49b080c
Roman Malynovskyi <malynovskyiroman@gmail.com>
2017-09-28 19:45:50 +0300

    3

---
+++ task2/Human.java
@@ -0,0 +1,61 @@
+package com.gmail.malynovskyiroman.task2;
+
+ class Human {
+    private String name;
+    private String surname;
+    private String gender;
+    private int age;
+
+    public Human(String name, String surname, String gender, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public Human() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
